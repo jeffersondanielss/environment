318b730f3a79d26a14d8882dfa316e02177b31e3
Jefferson Daniel <jeffersondanielss@gmail.com>
2018-02-04 13:12:35 -0200

    feat: show environment content by sidebar click

--- src/javascript/components/sidebarItem/index.js
+++ src/javascript/components/sidebarItem/index.js
@@ -1,5 +1,10 @@
 const {environments} = require(`../../../../config`)
 const {iconEnvironment} = require('../icons')
+const {
+  showElement,
+  arrayFromElements,
+  hideElement
+} = require('../../helpers/DOM')
 
 const component = environmentName => `
   <button class="sidebar__item" title="${environmentName}" data-id="${environmentName}">
@@ -12,8 +17,38 @@ const menuitem = () =>
     .join(',')
     .replace(/,/g, '')
 
+const showContent = envName => {
+  const element = document.querySelector(`[data-id=${envName}]`);
+
+  element.addEventListener('click', () => {
+    hideAllContents(envName)
+    showElement(envName)
+  })
+}
+
+const showFirstContent = () => {
+  document
+    .querySelectorAll('.content')[0]
+    .classList
+    .add('active')
+}
+
+const hideAllContents = envName => {
+  contents = arrayFromElements('.content')
+  contents.map(element => hideElement(element))
+}
+
+const onClick = () => {
+  Object.keys(environments)
+    .map(current => showContent(current))
+}
+
 const sidebar = () => {
-  document.querySelector('.sidebar__buttons').innerHTML = menuitem()
+  const sidebar = document.querySelector('.sidebar__buttons')
+
+  sidebar.innerHTML = menuitem()
+  showFirstContent()
+  onClick()
 }
 
 module.exports = sidebar
