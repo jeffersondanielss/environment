12e17355985edc15a95a2af92b0cff563fd85806
Jefferson Daniel <jdsilva@sp.r7.com>
2018-02-05 14:39:24 -0200

    fix: catch service errors

--- src/javascript/actions/service.js
+++ src/javascript/actions/service.js
@@ -5,11 +5,14 @@ const service = async options => {
   
   const [serviceName, serviceCommand, element, host ] = options
   const serviceCmd = `sudo service ${serviceName} ${serviceCommand}`
-  
-  
   beforeload(element)
-  const data = await command(serviceCmd, host)
-  document.querySelector(element).innerHTML = data.toString()
+
+  try {
+    const data = await command(serviceCmd, host)
+    document.querySelector(element).innerHTML = data.toString()
+  } catch (error) {
+    document.querySelector(element).innerHTML = error
+  }
 }
 
 module.exports = service
